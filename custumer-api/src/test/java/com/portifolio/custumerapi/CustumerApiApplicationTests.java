a7a091cb108dbe4ef6345a44b6ecb0031b765ba4
Carlos Barros <carlosuntr@gmail.com>
2023-03-30 11:30:39 -0300

    Including project's dependencies and structures

---
+++ custumer-api/src/test/java/com/portifolio/custumerapi/CustumerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.custumerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustumerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
